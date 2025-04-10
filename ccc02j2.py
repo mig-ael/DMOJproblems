6ea47d7357dde560fd8f316f93993dd7404bff77
mig-ael <dupreezmigael@gmail.com>
2025-04-10 13:27:10 -0700

    resubmit

--- ccc02j2.py
+++ ccc02j2.py
@@ -1,7 +1,6 @@
 # AmeriCanadian
 
 vowels=["a","e","i","o","u","y"]
-
 word=input()
 while word!='quit!':
     if len(word)>4 and word[len(word)-1-2]!=vowels and word[len(word)-1]=='r' and word[len(word)-2]=='o':
