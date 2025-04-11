27040fe8ea264766ba7482b5d405d8df48e1e85d
mig-ael <dupreezmigael@gmail.com>
2025-04-10 19:25:42 -0700

    ameriCanadian correct, 15/15, 3pt

--- ccc02j2.py
+++ ccc02j2.py
@@ -2,13 +2,18 @@
 
 vowels=["a","e","i","o","u","y"]
 word=input()
-while word!='quit!':
-    if len(word)>4 and word[len(word)-1-2]!=vowels and word[len(word)-1]=='r' and word[len(word)-2]=='o':
+words=[]
+def can(word):
+    if len(word)>4 and word[len(word)-1]=='r' and word[len(word)-2]=='o' and word[len(word)-2-1] not in vowels:
         for i in range(len(word)-1):
             print(word[i],end='')
         print("ur")
     else:
-        print(word)
+        print(word)    
+
+while word!='quit!':
+    words.append(word)
     word=input()
-    
-    
+
+for word in words:
+    can(word)
