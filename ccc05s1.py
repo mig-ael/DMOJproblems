3238171e51582cac1a622c4e6cdc5b63b3043136
mig-ael <dupreezmigael@gmail.com>
2025-04-16 21:54:39 -0700

    15/15 5pt correct Snow Calls s1 -used list.pop() and list.insert(), struggled with string printing so switched to arrays. Used 2d Array

--- ccc05s1.py
+++ ccc05s1.py
@@ -1,16 +1,27 @@
 # Snow Calls
 
 keypad=[["A","B","C"],["D","E","F"],["G","H","I"],["J","K","L"],["M","N","O"],["P","Q","R","S"],["T","U","V"],["W","X","Y","Z"]]
-exclude=[1,2,3,4,5,6,7,8,9,0]
 ammount=int(input())
-
+s=''
+a=[]
+total=[]
 for i in range(ammount):
     s=input()
+
     for char in s:
-        if (char not in exclude) or char!="-":
+            if char !='-':
+                a.append(str(char))
             for num in keypad:
                 for letter in num:
                     if char==letter:
-                        print(keypad.index(num)+2,end='')
-        else:
-            print(char,end='')
+                        a.pop()
+                        a.append(keypad.index(num)+2)
+    a.insert(3,'-')
+    a.insert(7,'-')
+    total.append(a)
+    a=[]
+for i in total:
+    for j in range(12):
+        
+        print(i[j],end='')
+    print()
