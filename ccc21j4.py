91c63c7426468c299266b4951eb29072c7587df6
mig-ael <dupreezmigael@gmail.com>
2025-04-29 13:46:02 -0700

    1/15 7pt Arranging Books. Swap logic is wrong, check logic is wrong, everything is wrong. Yet based on my own tests my stuff works? Learned that lists are pointers and updates previous list (start=books, if books is changed, so is start). Fixed this by doing books=start[:] which sends all of the information in the list instead of pointing to the list. 2/10

--- ccc21j4.py
+++ ccc21j4.py
@@ -1,6 +1,7 @@
 # Arranging Books
 
-books=list(input())
+start=list(input())
+books=start[:]
 L=0
 M=0
 for char in books:
@@ -9,35 +10,47 @@ for char in books:
     if char=='M':
         M+=1
 def large():
-    countL=0
     for j in range(len(books)):
         for i in range(len(books)):
             if books[i]=='L' and i>0:
                 books.pop(i)
                 books.insert(i-1,"L")
-                countL+=1
 
-
-    return countL//i
 
 def med():
-    countM=0
     for j in range(len(books)):
         for i in range(len(books)):
             if books[i]=='M' and i>0:
                 books.pop(i)
                 books.insert(i-1,"M")
-                countM+=1
 
 
-    return countM//i
-
+def check():
+    tempL=0
+    tempM=0
+    if books[:]!=start[:]:
+        tempL=1
+        tempM=1
+        for i in range(len(books)):
+            if books[i]!='L' and start[i]!='L' and (books[i]!='M' and start[i]!='M'):
+                tempL+=1
+            if books[i]!='M' and start[i]!='M' and (books[i]!='L' and start[i]!='L'):
+                tempM+=1
+    return tempL+tempM
 
 if L>0 and M>0:
-    print(med()+large())
+    med()
+    large()
+    
+    print(check())
 
 elif L>0 and M==0:
-    print(large())
+    large()
+    print(check())
 
 elif L==0 and M>0:
-    print(med())
+    med()
+    print(check())
+
+else:
+    print(0)
